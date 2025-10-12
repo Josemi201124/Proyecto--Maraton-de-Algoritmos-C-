6e395b49c2bf8ba859432bfaf2c60aa91738f8f8
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:14:54 -0500

    Add files via upload

---
+++ EjercicioC89.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//89. Calcular el perímetro de un triángulo.
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b, c;
+    cout << "Ingrese los lados del triángulo: "; cin >> a >> b >> c;
+    double perimetro = a + b + c;
+    cout << "Perímetro del triángulo: " << perimetro << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
