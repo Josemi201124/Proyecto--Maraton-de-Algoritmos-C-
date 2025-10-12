025d67ca356b2d20d2f5f574dc7f873520a477a8
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:35:19 -0500

    Add files via upload

---
+++ EjercicioC52.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//52. Calcular la media aritmética de cinco números.
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b, c, d, e;
+    cout << "Ingrese cinco números: ";
+    cin >> a >> b >> c >> d >> e;
+    double media = (a + b + c + d + e) / 5;
+    cout << "Media aritmética: " << media << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
