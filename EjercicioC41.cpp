43c1a7b8ba97abf20ea577a19b9a1e4e0abff6ad
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:51:30 -0500

    Add files via upload

---
+++ EjercicioC41.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//41. Calcular el tiempo si se conoce velocidad y distancia.
+#include <iostream>
+using namespace std;
+int main() {
+    double distancia, velocidad;
+    cout << "Distancia (km): "; cin >> distancia;
+    cout << "Velocidad (km/h): "; cin >> velocidad;
+    cout << "Tiempo = " << distancia / velocidad << " horas." << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
