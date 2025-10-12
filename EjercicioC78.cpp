7aa6373285612452943a74e5d58597dab511f1d8
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:07:28 -0500

    Add files via upload

---
+++ EjercicioC78.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//78. Convertir kilogramos a libras.
+#include <iostream>
+using namespace std;
+int main() {
+    double kg;
+    cout << "Ingrese kilogramos: "; cin >> kg;
+    double libras = kg / 0.453592;
+    cout << "Equivalente en libras: " << libras << endl;
+    return 0;
+}
+
+
+
+
+
+
