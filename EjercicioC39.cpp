0884d1703b6f41188a26f3687523c236cc0a3a18
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:50:31 -0500

    Add files via upload

---
+++ EjercicioC39.cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//39. Calcular el perímetro de un triángulo equilátero.
+#include <iostream>
+using namespace std;
+int main() {
+    double lado;
+    cout << "Lado del triángulo: ";
+    cin >> lado;
+    cout << "Perímetro: " << lado * 3 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
