59c04cd46b907ffcb43147755ce3594501ca2e49
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:58:04 -0500

    Add files via upload

---
+++ EjercicioC50.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//50. Mostrar el cuadrado y el cubo de un número.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double n;
+    cout << "Ingrese un número: ";
+    cin >> n;
+    cout << "Cuadrado: " << pow(n, 2) << ", Cubo: " << pow(n, 3) << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
