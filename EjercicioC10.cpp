1f3ac303fc38ca0d5cd5cfba73054517c51aa5c2
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:47:57 -0500

    Add files via upload

---
+++ EjercicioC10.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//10. Restar dos números.
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b;
+    cout << "Ingrese dos números: ";
+    cin >> a >> b;
+    cout << "La resta es: " << a - b << endl;
+    return 0;
+}
+
+
+
+
+
+
