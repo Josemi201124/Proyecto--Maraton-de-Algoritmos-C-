fb87b4d7256fbb6eeebab690c95092d44d8d78a7
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:23:24 -0500

    Add files via upload

---
+++ EjercicioC97.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//97. Calcular el área de un triángulo a partir de base y altura.
+#include <iostream>
+using namespace std;
+int main() {
+    double base, altura;
+    cout << "Base: "; cin >> base;
+    cout << "Altura: "; cin >> altura;
+    double area = (base * altura) / 2;
+    cout << "Área del triángulo: " << area << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
