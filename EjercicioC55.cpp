7b0fbb4d3753b9ec32e04784bc62bfe4b82dcadd
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:38:15 -0500

    Add files via upload

---
+++ EjercicioC55.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//55. Convertir dólares a euros.
+#include <iostream>
+using namespace std;
+int main() {
+    double dolares, tasa;
+    cout << "Ingrese cantidad en dólares: "; cin >> dolares;
+    cout << "Tasa de cambio (1 dólar a euros): "; cin >> tasa;
+    cout << "Equivalente en euros: " << dolares * tasa << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
