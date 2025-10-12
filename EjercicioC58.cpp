7fdac6df8515550c5c41681e98c1bae2d91c6a8b
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:41:48 -0500

    Add files via upload

---
+++ EjercicioC58.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//58. Calcular el perímetro y área de un rectángulo.
+#include <iostream>
+using namespace std;
+int main() {
+    double base, altura;
+    cout << "Base: "; cin >> base;
+    cout << "Altura: "; cin >> altura;
+    cout << "Perímetro: " << 2 * (base + altura) << endl;
+    cout << "Área: " << base * altura << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
