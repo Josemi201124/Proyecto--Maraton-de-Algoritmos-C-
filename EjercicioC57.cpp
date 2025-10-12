44caba23325b9dd5efa7f21730f1e93ce94485e4
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:41:05 -0500

    Add files via upload

---
+++ EjercicioC57.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//57. Mostrar el doble y triple de un número.
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingrese un número: "; cin >> n;
+    cout << "Doble: " << n * 2 << ", Triple: " << n * 3 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
