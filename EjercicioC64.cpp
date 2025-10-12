e335c463fb0cfa4019d59fcfcd017bd85c34e91f
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:50:52 -0500

    Add files via upload

---
+++ EjercicioC64.cpp
@@ -0,0 +1,33 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//64. Convertir una cantidad en horas a segundos.
+#include <iostream>
+using namespace std;
+int main() {
+    double horas;
+    cout << "Ingrese horas: "; cin >> horas;
+    cout << "Equivalente en segundos: " << horas * 3600 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
