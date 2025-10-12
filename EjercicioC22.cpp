b80a7946c72cde36a67c1f815b52a4b83b2acde0
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:02:20 -0500

    Add files via upload

---
+++ EjercicioC22.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//22. Convertir kilómetros a millas.
+#include <iostream>
+using namespace std;
+int main() {
+    double km;
+    cout << "Kilómetros: ";
+    cin >> km;
+    cout << "Millas: " << km * 0.621371 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
