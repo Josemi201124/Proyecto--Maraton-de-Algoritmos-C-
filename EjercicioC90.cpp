32a34820fab260f850593256f66a67393e9808e0
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:15:39 -0500

    Add files via upload

---
+++ EjercicioC90.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//90. Calcular el área de un rectángulo.
+#include <iostream>
+using namespace std;
+int main() {
+    double base, altura;
+    cout << "Base: "; cin >> base;
+    cout << "Altura: "; cin >> altura;
+    double area = base * altura;
+    cout << "Área del rectángulo: " << area << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
