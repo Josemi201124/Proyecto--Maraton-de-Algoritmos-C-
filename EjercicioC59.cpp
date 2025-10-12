bd2e2fbdd782189da2112e7169045e053903b715
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:42:26 -0500

    Add files via upload

---
+++ EjercicioC59.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//59. Calcular el promedio de edad de tres personas.
+#include <iostream>
+using namespace std;
+int main() {
+    int e1, e2, e3;
+    cout << "Ingrese tres edades: ";
+    cin >> e1 >> e2 >> e3;
+    cout << "Promedio de edad: " << (e1 + e2 + e3) / 3.0 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
