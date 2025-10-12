d336197bd4009944e2db23a98cb892a6fbfaf483
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:53:09 -0500

    Add files via upload

---
+++ EjercicioC68.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//68. Convertir años a días y meses (aproximado).
+#include <iostream>
+using namespace std;
+int main() {
+    int años;
+    cout << "Ingrese años: "; cin >> años;
+    cout << "Equivale a " << años * 12 << " meses o " << años * 365 << " días." << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
