b14b8db74a6277c4bcf699cc11816dbaaa9cfa38
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:53:29 -0500

    Add files via upload

---
+++ EjercicioC13.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//13. Calcular el promedio de tres números.
+#include <iostream>
+using namespace std;
+int main() {
+    double x, y, z;
+    cout << "Ingrese tres números: ";
+    cin >> x >> y >> z;
+    double promedio = (x + y + z) / 3; // Se suman y dividen entre tres
+    cout << "El promedio es: " << promedio << endl;
+    return 0;
+}
+
+
+
+
+
+
