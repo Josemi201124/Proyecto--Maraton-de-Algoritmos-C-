2166b10b03da6e234034b1dfcfbc15da7eb8dcb7
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:40:03 -0500

    Add files via upload

---
+++ EjercicioC56.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//56. Calcular el costo total de artículos comprados.
+#include <iostream>
+using namespace std;
+int main() {
+    double precioUnitario;
+    int cantidad;
+    cout << "Precio unitario: "; cin >> precioUnitario;
+    cout << "Cantidad comprada: "; cin >> cantidad;
+    cout << "Total a pagar: $" << precioUnitario * cantidad << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
