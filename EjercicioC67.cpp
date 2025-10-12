2ea863ed63b544bfaa8f872046aa3495082ff071
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:52:30 -0500

    Add files via upload

---
+++ EjercicioC67.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//67. Calcular la energía cinética.
+#include <iostream>
+using namespace std;
+int main() {
+    double masa, velocidad;
+    cout << "Masa (kg): "; cin >> masa;
+    cout << "Velocidad (m/s): "; cin >> velocidad;
+    cout << "Energía cinética: " << 0.5 * masa * velocidad * velocidad << " J" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
