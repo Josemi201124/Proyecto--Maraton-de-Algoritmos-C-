e74e9708428c5e43c556b77ecf7ac6099e2bdad0
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:57:17 -0500

    Add files via upload

---
+++ EjercicioC49.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//49. Mostrar el número anterior y posterior al ingresado.
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Ingrese un número: ";
+    cin >> n;
+    cout << "Número anterior: " << n - 1 << ", posterior: " << n + 1 << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
