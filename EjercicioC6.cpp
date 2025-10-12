d63cc8f87c58fe32e4f660b4e6cc9315a6b50d8a
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:41:37 -0500

    Add files via upload

---
+++ EjercicioC6.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//6. Calcular el área de un rectángulo.
+#include <iostream>
+using namespace std;
+int main() {
+    double largo, ancho;
+    cout << "Largo: ";
+    cin >> largo;
+    cout << "Ancho: ";
+    cin >> ancho;
+    cout << "Área: " << largo * ancho << endl;
+    return 0;
+}
+
+
