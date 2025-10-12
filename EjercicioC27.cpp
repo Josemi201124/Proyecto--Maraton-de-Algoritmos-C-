c47b2df267bc7957297a52f85926449a2f23cadf
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:33:47 -0500

    Add files via upload

---
+++ EjercicioC27.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//27. Calcular el área de un paralelogramo.
+#include <iostream>
+using namespace std;
+int main() {
+    double base, altura;
+    cout << "Base: ";
+    cin >> base;
+    cout << "Altura: ";
+    cin >> altura;
+    cout << "Área del paralelogramo: " << base * altura << endl;
+    return 0;
+}
+
