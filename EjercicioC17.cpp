bf7641b1e86d83a628888c2a0e30a20cbefa3e56
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:57:26 -0500

    Add files via upload

---
+++ EjercicioC17.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//17. Convertir minutos a segundos.
+#include <iostream>
+using namespace std;
+int main() {
+    int minutos;
+    cout << "Ingrese minutos: ";
+    cin >> minutos;
+    cout << minutos << " minutos equivalen a " << minutos * 60 << " segundos." << endl;
+    return 0;
+}
+
+
+
+
+
+
+
