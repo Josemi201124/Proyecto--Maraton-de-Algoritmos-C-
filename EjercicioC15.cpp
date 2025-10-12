81fa3de986500dc618b3df05df246f2eff7c1858
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:55:42 -0500

    Add files via upload

---
+++ EjercicioC15.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//15. Calcular el área de un cuadrado.
+#include <iostream>
+using namespace std;
+int main() {
+    double lado;
+    cout << "Ingrese el lado del cuadrado: ";
+    cin >> lado;
+    cout << "Área: " << lado * lado << endl; // lado²
+    return 0;
+}
+
+
+
+
+
+
