c0bb218f30c23d7ed634a8d3a03bf1c46d7ba888
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:52:43 -0500

    Add files via upload

---
+++ EjercicioC43.cpp
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//43. Calcular el consumo de combustible.
+#include <iostream>
+using namespace std;
+int main() {
+    double distancia, litros;
+    cout << "Distancia recorrida (km): "; cin >> distancia;
+    cout << "Combustible consumido (L): "; cin >> litros;
+    cout << "Consumo promedio: " << distancia / litros << " km/L" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
