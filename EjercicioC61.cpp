86a1f0889444b324da17bc8a010d7d531289417a
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 15:48:22 -0500

    Add files via upload

---
+++ EjercicioC61.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//61. Calcular la velocidad promedio.
+#include <iostream>
+using namespace std;
+int main() {
+    double distancia, tiempo;
+    cout << "Distancia (km): "; cin >> distancia;
+    cout << "Tiempo (h): "; cin >> tiempo;
+    cout << "Velocidad promedio: " << distancia / tiempo << " km/h" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
