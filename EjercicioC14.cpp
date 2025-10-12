e74145d40353ea795f7384e078a229fa606e41cb
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:54:24 -0500

    Add files via upload

---
+++ EjercicioC14.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//14. Calcular el perímetro de un cuadrado.
+#include <iostream>
+using namespace std;
+int main() {
+    double lado;
+    cout << "Ingrese el lado del cuadrado: ";
+    cin >> lado;
+    cout << "Perímetro: " << 4 * lado << endl; // 4 lados iguales
+    return 0;
+}
+
+
+
+
+
