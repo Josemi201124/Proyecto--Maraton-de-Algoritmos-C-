01ce8599e60de456a9a2f7cf13941e6efa49f4aa
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:44:49 -0500

    Add files via upload

---
+++ EjercicioC36.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//36. Convertir kilogramos a gramos.
+#include <iostream>
+using namespace std;
+int main() {
+    double kg;
+    cout << "Kilogramos: ";
+    cin >> kg;
+    cout << kg << " kg equivalen a " << kg * 1000 << " g." << endl;
+    return 0;
+}
+
+
+
+
+
+
+
