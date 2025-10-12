2bc535335c704a55cdcb316ba14f5315cd94d24a
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:41:08 -0500

    Add files via upload

---
+++ EjercicioC34.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//34. Convertir metros a centímetros.
+#include <iostream>
+using namespace std;
+int main() {
+    double m;
+    cout << "Metros: ";
+    cin >> m;
+    cout << m << " m equivalen a " << m * 100 << " cm." << endl;
+    return 0;
+}
+
+
+
+
