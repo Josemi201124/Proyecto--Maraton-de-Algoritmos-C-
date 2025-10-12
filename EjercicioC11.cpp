466ce1ef76309991a16bfcb10f3c779a366242fa
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:49:12 -0500

    Add files via upload

---
+++ EjercicioC11.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//11. Multiplicar dos números.
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b;
+    cout << "Ingrese dos números: ";
+    cin >> a >> b;
+    cout << "El producto es: " << a * b << endl;
+    return 0;
+}
+
+
+
+
+
