5bbb21f519d2e290cd6a54623218a8892e6547d5
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:17:59 -0500

    Add files via upload

---
+++ EjercicioC91.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//91. Calcular el promedio de cuatro números.
+#include <iostream>
+using namespace std;
+int main() {
+    double a, b, c, d;
+    cout << "Ingrese cuatro números: "; cin >> a >> b >> c >> d;
+    double promedio = (a + b + c + d) / 4;
+    cout << "Promedio: " << promedio << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
