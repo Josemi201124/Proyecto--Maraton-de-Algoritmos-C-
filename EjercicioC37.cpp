820b5e4f2622f06f2dbdbad4e46e53507578ec97
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 14:47:23 -0500

    Add files via upload

---
+++ EjercicioC37.cpp
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//37. Calcular el total a pagar por litros de gasolina.
+#include <iostream>
+using namespace std;
+int main() {
+    double precioLitro, litros;
+    cout << "Precio por litro: "; cin >> precioLitro;
+    cout << "Litros consumidos: "; cin >> litros;
+    cout << "Total a pagar: $" << precioLitro * litros << endl;
+    return 0;
+}
+
+
+
+
+
+
+
