b97bc89e1cc58c1f6407f2b66cde42b0cd5aa04c
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 13:56:25 -0500

    Add files via upload

---
+++ EjercicioC16.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//16. Convertir horas a minutos.
+#include <iostream>
+using namespace std;
+int main() {
+    int horas;
+    cout << "Ingrese horas: ";
+    cin >> horas;
+    cout << horas << " horas equivalen a " << horas * 60 << " minutos." << endl;
+    return 0;
+}
+
+
+
+
+
+
