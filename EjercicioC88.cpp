6f8b29cc285405c3b630193fc391cc4e3dcd12c5
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:14:20 -0500

    Add files via upload

---
+++ EjercicioC88.cpp
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//88. Calcular el perímetro de un rectángulo.
+#include <iostream>
+using namespace std;
+int main() {
+    double base, altura;
+    cout << "Base: "; cin >> base;
+    cout << "Altura: "; cin >> altura;
+    double perimetro = 2 * (base + altura);
+    cout << "Perímetro del rectángulo: " << perimetro << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
