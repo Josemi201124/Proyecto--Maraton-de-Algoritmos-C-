12fdf24c8cff7771ebd7ded225fbe49b2bc02309
GloderRdgHn <grodriguez@alumno.montepiedra.edu.ec>
2025-10-12 16:21:42 -0500

    Add files via upload

---
+++ EjercicioC94.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+//Gloder Rodriguez
+//94. Convertir millas a kilómetros.
+#include <iostream>
+using namespace std;
+int main() {
+    double millas;
+    cout << "Ingrese millas: "; cin >> millas;
+    double km = millas / 0.621371;
+    cout << "Equivalente en kilómetros: " << km << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
